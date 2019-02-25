ac0d36d4be5909ae475699a52b7897f4d3b60644
relaxslow <relaxslow@hotmail.com>
2019-02-24 23:02:09 -0500

    u

--- common/code.js
+++ common/code.js
@@ -1,5 +1,5 @@
 
-function format(code,fun) {
+function format(code, fun) {
     let fileName = code.dataset.file;
     if (!fileName) return;
     let req = new XMLHttpRequest();
@@ -7,11 +7,32 @@ function format(code,fun) {
     req.onload = codeRecieved;
     req.send();
     function codeRecieved() {
+       
+        // Options for the observer (which mutations to observe)
+        let config = { attributes: true, childList: true, subtree: true };
+
+        // Callback function to execute when mutations are observed
+        let  callback = function (mutationsList, observer) {
+            for (var mutation of mutationsList) {
+                if (mutation.type == 'childList') {
+                     fun();
+//                     console.log('A child node has been added or removed.');
+                    observer.disconnect();
+                }
+                else if (mutation.type == 'attributes') {
+//                     console.log('The ' + mutation.attributeName + ' attribute was modified.');
+                }
+            }
+        };
+
+        // Create an observer instance linked to the callback function
+        let  observer = new MutationObserver(callback);
+        observer.observe(code, config);
+
         code.innerHTML = req.response;
-        requestAnimationFrame(function(){
-            fun();
-        })
-        
+       
+
+
     }
 
     enableDrag(code);
